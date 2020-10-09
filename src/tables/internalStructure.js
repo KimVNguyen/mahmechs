3c8331bce9a10f3dc3665aff88862f83d849067d
Kim Nguyen <kimchimik@gmail.com>
2020-10-09 10:15:06 -0700

    adding internal structure table

---
+++ src/tables/internalStructure.js
@@ -0,0 +1,21 @@
+module.exports = {
+    table: {
+        "20":{"head":"3","center_torso":"6","left_torso":"5","right_torso":"5","left_arm":"3","right_arm":"3","left_leg":"4","right_leg":"4","max_armor":"69/73"},
+        "25":{"head":"3","center_torso":"8","left_torso":"6","right_torso":"6","left_arm":"4","right_arm":"4","left_leg":"6","right_leg":"6","max_armor":"89/97"},
+        "30":{"head":"3","center_torso":"10","left_torso":"7","right_torso":"7","left_arm":"5","right_arm":"5","left_leg":"7","right_leg":"7","max_armor":"105/113"},
+        "35":{"head":"3","center_torso":"11","left_torso":"8","right_torso":"8","left_arm":"6","right_arm":"6","left_leg":"8","right_leg":"8","max_armor":"119/127"},
+        "40":{"head":"3","center_torso":"12","left_torso":"10","right_torso":"10","left_arm":"6","right_arm":"6","left_leg":"10","right_leg":"10","max_armor":"137/153"},
+        "45":{"head":"3","center_torso":"14","left_torso":"11","right_torso":"11","left_arm":"7","right_arm":"7","left_leg":"11","right_leg":"11","max_armor":"153/169"},
+        "50":{"head":"3","center_torso":"16","left_torso":"12","right_torso":"12","left_arm":"8","right_arm":"8","left_leg":"12","right_leg":"12","max_armor":"169/185"},
+        "55":{"head":"3","center_torso":"18","left_torso":"13","right_torso":"13","left_arm":"9","right_arm":"9","left_leg":"13","right_leg":"13","max_armor":"185/201"},
+        "60":{"head":"3","center_torso":"20","left_torso":"14","right_torso":"14","left_arm":"10","right_arm":"10","left_leg":"14","right_leg":"14","max_armor":"201/217"},
+        "65":{"head":"3","center_torso":"21","left_torso":"15","right_torso":"15","left_arm":"10","right_arm":"10","left_leg":"15","right_leg":"15","max_armor":"211/231"},
+        "70":{"head":"3","center_torso":"22","left_torso":"15","right_torso":"15","left_arm":"11","right_arm":"11","left_leg":"15","right_leg":"15","max_armor":"217/233"},
+        "75":{"head":"3","center_torso":"23","left_torso":"16","right_torso":"16","left_arm":"12","right_arm":"12","left_leg":"16","right_leg":"16","max_armor":"231/247"},
+        "80":{"head":"3","center_torso":"25","left_torso":"17","right_torso":"17","left_arm":"13","right_arm":"13","left_leg":"17","right_leg":"17","max_armor":"247/263"},
+        "85":{"head":"3","center_torso":"27","left_torso":"18","right_torso":"18","left_arm":"14","right_arm":"14","left_leg":"18","right_leg":"18","max_armor":"263/279"},
+        "90":{"head":"3","center_torso":"29","left_torso":"19","right_torso":"19","left_arm":"15","right_arm":"15","left_leg":"19","right_leg":"19","max_armor":"279/295"},
+        "95":{"head":"3","center_torso":"30","left_torso":"20","right_torso":"20","left_arm":"16","right_arm":"16","left_leg":"20","right_leg":"20","max_armor":"293/309"},
+        "100":{"head":"3","center_torso":"31","left_torso":"21","right_torso":"21","left_arm":"17","right_arm":"17","left_leg":"21","right_leg":"21","max_armor":"307/323"}
+    }
+}
