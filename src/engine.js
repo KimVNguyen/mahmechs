bcdcd3ffbd8de794c17e72ac73d08d58b7671a7c
Kim Nguyen <kimchimik@gmail.com>
2020-11-07 12:53:38 -0800

    updated wording on tests and impletmended functions

---
+++ src/engine.js
@@ -0,0 +1,13 @@
+const data = require ('./tables/engine.js').table;
+
+module.exports = {
+    getTable: (rating) => {
+        return data[rating];
+    },
+    calculateRating: (tonnage, mp )=> {
+        return (tonnage * mp)
+    }
+
+
+
+}
--- test/engine.js
+++ test/engine.js
@@ -1,11 +1,13 @@
 const chai = require('chai');
+const engine = require('../src/engine')
 const assert = chai.assert;
+
 
 describe('Engine Functions', function () {
     describe("Table Lookup", function () {
-        it ('should return the correct values for the specified tonnage', function() {
-            const record = {}
-            assert.deepEqual(record, {'120': {
+        it ('should return the correct values for the specified engine rating', function() {
+            const record = engine.getTable("120")
+            assert.deepEqual(record, {
                 ICE: '8.0',
                 Cell: '5.0',
                 Fission: '7.0',
@@ -13,17 +15,17 @@ describe('Engine Functions', function () {
                 Std: '4.0',
                 Light: '3.0',
                 XL: '2.0'
-              }
+        
             })
         });
-        it ('should return the undefined for invalid tonnage', function() {
-            const record = {}
+        it ('should return the undefined for invalid engine rating', function() {
+            const record = engine.getTable("700")
             assert.equal(record, undefined);
         });
     });
     describe("Calculations", function () {
-        it ('should calculate engine reating based upon walking speed and tonnage', function() {
-            const rating = undefined
+        it ('should calculate engine raSting based upon walking speed and tonnage', function() {
+            const rating = engine.calculateRating(50,6)
             assert.equal(rating, 300);
         });
     });
