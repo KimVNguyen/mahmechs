b2e68d4085cb6570c7c554599a264897e8082c0e
Kim Nguyen <kimchimik@gmail.com>
2020-11-09 21:36:11 -0800

    changing round to ceil

--- src/helpers/calculations.js
+++ src/helpers/calculations.js
@@ -2,7 +2,7 @@ module.exports = {
     round: (value, step) => {
         step || (step = 1.0);
         var inv = 1.0 / step;
-        return Math.round(value * inv) / inv;
+        return Math.ceil(value * inv) / inv;
     }
 
 
